85a12cebdef555389a5cf6f6961aca06c92a8ae1
Juan Carlos <juancagb.17@gmail.com>
2016-10-23 20:32:41 -0500

    Firebase Cloud Messaging

--- app/src/main/java/com/youtube/sorcjc/fullday2016/MainActivity.java
+++ app/src/main/java/com/youtube/sorcjc/fullday2016/MainActivity.java
@@ -2,6 +2,10 @@ package com.youtube.sorcjc.fullday2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.youtube.sorcjc.fullday2016.io.fcm.MyFirebaseInstanceIDService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MyFirebase", FirebaseInstanceId.getInstance().getToken());
     }
 }
